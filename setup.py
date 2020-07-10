18d06427eaa2668ce81f40f1cead7ee12e8ed164
Arthur Sugden <arthur.sugden@gmail.com>
2020-07-10 09:40:57 -0400

    Added automatic searching for packages.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ scripts = []
 
 setup(
     name="popgen_utils",
-    version="0.0.1",
+    version="0.0.2",
     author='Lauren Sugden & Arthur Sugden',
     packages=setuptools.find_packages(),
     install_requires=[
